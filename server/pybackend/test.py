11ded1f91f7db7f40ba1be86608d9645a0a5d392
pasqualegravante <p.gravante18@gmail.com>
2026-02-20 00:53:36 +0100

    Remove - temporary files

--- server/pybackend/test.py
+++
@@ -1,6 +0,0 @@
-import urbangraph as ug
-
-graph = ug.UrbanGraph()
-
-# lettura di file .csv da cui creare l'istanza di grafo da eseguire
-# 
--- server/pybackend/urbangraph.py
+++
@@ -1,6 +0,0 @@
-import networkx as nx
-
-class UrbanGraph:
-    def __init__(self):
-        self.graph = nx.DiGraph()
-    
